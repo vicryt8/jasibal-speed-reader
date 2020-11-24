2e94e5651ac072e43ef639afe94dc7cac25bc7b7
josaye <j.m.asibal@hotmail.com>
2020-11-24 23:17:03 +1100

    consolidated functions

--- speedReader.js
+++ speedReader.js
@@ -18,7 +18,7 @@ I don't want to die \
 I sometimes wish I'd never been born at all";
 var timerString = "The Speed Reading will begin in: ";
 var speedReadWord = speedReadString.split(" ");
-var userSpeedInput = prompt("Please enter a number between 1 and 10");
+var userSpeedInput = prompt("Please enter a number between 1 (slow) and 10 (fast)");
 var userSpeed = parseInt(userSpeedInput);
 var h1Element = document.createElement("h1");
 h1Element.setAttribute("id", "updateDiv");
@@ -28,28 +28,26 @@ var s = 5;
 var w = 0;
 //5 second timer
 function countdown() {
-    displayCountdown(); //update html
-    if (s > 0) { //if seconds doesn't equal 0
+    display(timerString + s); //update html
+    if (s !== 0) { //if seconds doesn't equal 0
         s--; // subtract 1 from s
         setTimeout(countdown, 1000); //wait 1 second and restart countdown function
+    }
+    else {
+        display(timerString + "GO!");
+        setTimeout(() => speedRead(), 1000); // when countdown reaches 0, start speed read function
     }
 }
 //speed reader 
 function speedRead() {
-    displayWord(); //update html
+    display(speedReadWord[w]); //update html
     if (w !== speedReadWord.length) { //if w doesnt equal number of words
         w++; // add 1 to w
         setTimeout(speedRead, 60000 / (60 * userSpeed)); // wait 60,000ms/(userInput*60ms)
     }
 }
-function displayCountdown() {
-    h1ElementElement.textContent = timerString + s; //updates h1 element text
-    if (s === 0) {
-        speedRead();
-    } // when countdown reaches 0, start speed read function
+function display(displayWord) {
+    h1ElementElement.textContent = displayWord; //updates h1 element text
 }
 ;
-function displayWord() {
-    h1ElementElement.textContent = speedReadWord[w]; //updates h1 element text
-}
 countdown();
